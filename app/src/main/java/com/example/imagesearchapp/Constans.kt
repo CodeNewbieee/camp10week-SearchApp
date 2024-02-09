a861e15752d376c8545639680d60a517056ec8de
CodeNewbieee <choi2232@hanmail.net>
2024-02-09 16:11:23 +0900

    [REFACTOR] API KEY 숨기기

--- app/src/main/java/com/example/imagesearchapp/Constans.kt
+++ app/src/main/java/com/example/imagesearchapp/Constans.kt
@@ -1,12 +1,10 @@
 package com.example.imagesearchapp
 
 object Constans {
-    const val KAKAO_API_KEY = "KakaoAK 3ea651ffcbc1122621375790dc07d854"
+    const val KAKAO_API_KEY = BuildConfig.API_KEY
     const val BASE_URL = "https://dapi.kakao.com/v2/search/"
     const val PREFS_FILENAME = "prefs"
     const val DEFAULT_STRING = ""
-    const val DEFAULT_INT = 0
-    const val DEFAULT_BOOLEAN = false
     const val KEY_INPUT = "inputdata"
     const val KEY_LOCKER = "lockerlist"
 }
