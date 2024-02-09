9f72f49f64df36775a0d873b1da77a93c4702e8e
CodeNewbieee <choi2232@hanmail.net>
2024-02-09 16:19:55 +0900

    [REFACTOR] API KEY 숨기기

--- app/src/main/java/com/example/imagesearchapp/Constans.kt
+++
@@ -1,8 +0,0 @@
-package com.example.imagesearchapp
-
-object Constans {
-    const val FAVORITE_DATA1 = "favorites1"
-    const val FAVORITE_DATA2 = "favorites2"
-    const val REQUEST_KEY1 = "requestKey1"
-    const val REQUEST_KEY2 = "requestKey2"
-}
--- app/src/main/java/com/example/imagesearchapp/Retrofit/KakaoAPI.kt
+++ app/src/main/java/com/example/imagesearchapp/Retrofit/KakaoAPI.kt
@@ -1,11 +1,12 @@
 package com.example.imagesearchapp.Retrofit
 
+import com.example.imagesearchapp.BuildConfig
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
-const val KAKAO_API_KEY = "KakaoAK 3ea651ffcbc1122621375790dc07d854"
+const val KAKAO_API_KEY = BuildConfig.API_KEY
 interface KakaoAPI {
     @GET("image")
     suspend fun getSearchImage (
